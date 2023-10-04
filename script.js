3622e52f01ff9182a065b31d32812da65f98d238
MuharremAlpnar <muharremaltinpinar@gmail.com>
2023-10-04 03:27:42 +0300

    soru ve cevaplar alındı

--- script.js
+++ script.js
@@ -4,84 +4,63 @@ function Soruolustur(soru, siklar, cevap) {
     this.cevap = cevap;
     
 }
-Soruolustur.prototype.cevapkontrol = function (yanit) {
-    return this.cevap === yanit;
-};
 
-const sorular=[
-    new Soruolustur(
-        "1866’da dinamiti icat eden ve bilim ve sanattaki büyük başarılar için soyadı ile anılan prestijli ödüllerin verilmesine ön ayak olan İsveçli bilim insanı kimdir?",
-        {
-            a:"Alfred Nobel",
-            b:"Louis Pasteur",
-            c:"Heinrich Herz",
-            d:"İsaac Newton"
-        },
-        "Alfred Nobel"
-    ),
-    new Soruolustur(
-        "1856-1939 yılları arasında yaşamış ve ünlü psikanaliz öğretisini geliştirmiş olan tanınmış Avusturyalı nörolog kimdir?",
-        {
-            a:"Alfred Nobel",
-            b:"Louis Pasteur",
-            c:"Sigmund Freud",
-            d:"Marie Curie"
-        },
-        "Sigmund Freud"
-    ),
-    new Soruolustur(
-        "Bilimkurgunun babası sayılan Fransız yazar aşağıdakilerden hangisidir?",
-        {
-            a:"Victor Hugo",
-            b:"Balzac",
-            c:"Jules Verne",
-            d:"Montaigne"
-        },
-        "Jules Verne"
-    ),
-    new Soruolustur(
-        "Antik çağda yaşamış, Batı Edebiyatı’nın ilk büyük eserleri sayılan İlyada ve Odessa Destanları’nın yaratıcısı veya derleyicisi olduğu kabul edilen kişi kimdir?",
-        {
-            a:"Sokrates",
-            b:"Homeros",
-            c:"Truva",
-            d:"Eflatun"
-        },
-        "Homeros"
-    ),
-
-]
-
-function Quiz(sorular) {
-    this.sorular = sorular;
-    this.index=0;
-}
-Quiz.prototype.sorugetir = function () {
-    return this.sorular[this.index]
-};
+const sorular=[]
 
 
-document.getElementById("buton").addEventListener("click",function(){
+document.getElementById("kayit").addEventListener("click",function(){
+    let sik = document.getElementsByClassName("şik");//ŞIKLAR
+    let soru = document.getElementById("soru").value;//textareadan gelen soru
+    let cevap = document.querySelector(".cevap").value;//cevap inputundan gelen cevap
 
-    let yapi = document.getElementById("soru").value    
+    // inputlardan gelen cevap şıklarını obje olarak döndüren fonksyon
+    function siklariAl(){
+        const siklar = {
+            a:"",
+            b:"",
+            c:"",
+            d:""
+        };
+        const dizi = [];
+        for (let i = 0; i < sik.length; i++) {
+            let s = sik[i].value
+            dizi.push(s)
+        };
+    
+        let i=0;
+        for (var key in siklar) {
+            siklar[key]=dizi[i]
+            i++;
+        }
 
+        return siklar;
+
+    }
+
+    //değerleri kaydettikten sonra sıfırlayacak fonksyon
+    function sifirla() {
+        for (let i = 0; i < sik.length; i++) {
+          sik[i].value = "";
+        }
+        document.getElementById("soru").value = ""; // textarea'yı sıfırla
+        document.querySelector(".cevap").value = ""; // input'u sıfırla
+      }
+   
+   
+
+    // soruoluştur constuctorundan bir nesne oluşturup sorular dizisine kaydettim
     sorular.push(
         new Soruolustur(
-            yapi,
-            {
-                a:"Alfred Nobel",
-                b:"Louis Pasteur",
-                c:"Heinrich Herz",
-                d:"İsaac Newton"
-            },
-            "Alfred Nobel"
+            soru,
+            siklariAl(),
+            cevap
         ),
     )
-    
 
+    sifirla();//kaydettikten sonra input ve textarea içlerini sıfırladım
+   
     
-    console.log(sorular)
-
+ 
 })
 
 
